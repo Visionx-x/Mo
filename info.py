90a6f8c2c939dddd577fe12aaae4b46d129611b0
Visionx-x <telegrambott19@gmail.com>
2024-12-12 21:42:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7048477254:AAHyqIpGE6eDpr_W50olLfyC5GOrVzj
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5016109398').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/innocent_babe_dead") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-4677368047'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002499839098'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/LB_Movies')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002480884401').split()]
 #---------------------------------------------------------------
